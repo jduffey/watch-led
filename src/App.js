be8f0d858f7787e11f793c4fcc3e5a8c6f663e6e
Jed Duffey <jedduffey@gmail.com>
2023-05-17 14:56:21 -0400

    WLED-1.1.3: LED grid size (#4)
    - LED grid gets size passed as props from App
    - Extract function in LedGrid for deciding if light should be on

--- src/App.js
+++ src/App.js
@@ -3,17 +3,10 @@ import './App.css';
 import LedGrid from './components/LedGrid';
 
 const App = () => {
-    const grid = [
-        [true, false, true, false],
-        [false, true, false, true],
-        [true, false, true, false],
-        [false, true, false, true],
-    ];
-
     return (
         <div className="App">
             <h1>LED Light Display</h1>
-            <LedGrid grid={grid} />
+            <LedGrid width={4} height={4} />
         </div>
     );
 }
--- src/components/LedGrid.js
+++ src/components/LedGrid.js
@@ -1,12 +1,21 @@
 import LedLight from './LedLight';
 
-const LedGrid = ({ grid }) => {
+const isLightOn = (a, b) => (a + b) % 2 === 0;
+
+const LedGrid = ({ width, height }) => {
+    const grid = Array(height)
+        .fill()
+        .map(() => Array(width).fill(null));
+
     return (
         <div>
             {grid.map((row, rowIndex) => (
                 <div key={rowIndex} style={{ display: 'flex' }}>
-                    {row.map((isOn, lightIndex) => (
-                        <LedLight key={lightIndex} isOn={isOn} />
+                    {row.map((_, colIndex) => (
+                        <LedLight
+                            key={colIndex}
+                            isOn={isLightOn(rowIndex, colIndex)}
+                        />
                     ))}
                 </div>
             ))}
