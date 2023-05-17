ca1655d54c863c3100af44ced1d6e02187731218
Jed Duffey <jedduffey@gmail.com>
2023-05-17 14:50:43 -0400

    LED grid gets size from passed props

--- src/components/LedGrid.js
+++ src/components/LedGrid.js
@@ -1,12 +1,16 @@
 import LedLight from './LedLight';
 
-const LedGrid = ({ grid }) => {
+const LedGrid = ({ width, height }) => {
+    const grid = Array(height)
+        .fill()
+        .map(() => Array(width).fill(null));
+
     return (
         <div>
             {grid.map((row, rowIndex) => (
                 <div key={rowIndex} style={{ display: 'flex' }}>
-                    {row.map((isOn, lightIndex) => (
-                        <LedLight key={lightIndex} isOn={isOn} />
+                    {row.map((_, colIndex) => (
+                        <LedLight key={colIndex} isOn={(rowIndex + colIndex) % 2 === 0 ? true : false} />
                     ))}
                 </div>
             ))}
