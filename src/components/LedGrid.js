a7d6611999f3bfdcc2ca1577fa994869fbedbce5
Jed Duffey <jedduffey@gmail.com>
2023-05-17 14:54:38 -0400

    Extract fn for deciding if light should be on

--- src/components/LedGrid.js
+++ src/components/LedGrid.js
@@ -1,5 +1,7 @@
 import LedLight from './LedLight';
 
+const isLightOn = (a, b) => (a + b) % 2 === 0;
+
 const LedGrid = ({ width, height }) => {
     const grid = Array(height)
         .fill()
@@ -10,7 +12,10 @@ const LedGrid = ({ width, height }) => {
             {grid.map((row, rowIndex) => (
                 <div key={rowIndex} style={{ display: 'flex' }}>
                     {row.map((_, colIndex) => (
-                        <LedLight key={colIndex} isOn={(rowIndex + colIndex) % 2 === 0 ? true : false} />
+                        <LedLight
+                            key={colIndex}
+                            isOn={isLightOn(rowIndex, colIndex)}
+                        />
                     ))}
                 </div>
             ))}
