eb6435ad993f922356bbfa628a164ac4651ba6ea
Jed Duffey <jedduffey@gmail.com>
2023-05-17 13:53:47 -0400

    Create LedLight component and hardcode two of them on App.js -- one on and one off

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LedLight from './components/LedLight';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>LED Light Display</h1>
+            <div>
+                <LedLight isOn={true} />
+                <LedLight isOn={false} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
