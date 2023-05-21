0135a65886164e994e4d884fe3848f326c9908e2
Jed Duffey <jedduffey@gmail.com>
2023-05-21 13:46:28 -0400

    Extract CharGrid component in LedGrid file

--- src/components/LedGrid.js
+++ src/components/LedGrid.js
@@ -2,6 +2,25 @@ import LedLight from './LedLight';
 
 const isLightOn = (x) => parseInt(x, 16) % 2 === 0;
 
+const CharGrid = ({ width, height, entropy }) => {
+    const grid = Array(height)
+        .fill()
+        .map(() => Array(width).fill(null));
+
+    return grid.map((row, rowIndex) => (
+        <div key={rowIndex} style={{ display: 'flex' }}>
+            {row.map((_, colIndex) => {
+                const ledIndex = rowIndex * width + colIndex;
+                return (
+                    <div className="LedGridContainerCharacter">
+                        {entropy[ledIndex]}
+                    </div>
+                );
+            })}
+        </div>
+    ));
+}
+
 const LedGrid = ({ width, height, entropy }) => {
     console.log("LedGrid", width, height, entropy);
 
@@ -26,18 +45,7 @@ const LedGrid = ({ width, height, entropy }) => {
                     })}
                 </div>
             ))}
-            {grid.map((row, rowIndex) => (
-                <div key={rowIndex} style={{ display: 'flex' }}>
-                    {row.map((_, colIndex) => {
-                        const ledIndex = rowIndex * width + colIndex;
-                        return (
-                            <div className="LedGridContainerCharacter">
-                                {entropy[ledIndex]}
-                            </div>
-                        );
-                    })}
-                </div>
-            ))}
+            <CharGrid width={width} height={height} entropy={entropy} />
         </div>
     );
 };
