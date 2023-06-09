a9086e69870709ca1c9647c5fb584065ce9d1724
Jed Duffey <jedduffey@gmail.com>
2023-06-09 09:46:18 -0400

    Spike script

---
+++ server/simple_thresholding.py
@@ -0,0 +1,26 @@
+import cv2
+
+
+image = cv2.imread('images/1686316695.png')
+cv2.imshow("Image", image)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+
+# apply basic thresholding -- the first parameter is the image
+# we want to threshold, the second value is is our threshold
+# check; if a pixel value is greater than our threshold (in this
+# case, 200), we set it to be *black, otherwise it is *white*
+(T, threshInv) = cv2.threshold(blurred, 159, 255,
+                               cv2.THRESH_BINARY_INV)
+
+cv2.imshow("Threshold Binary Inverse", threshInv)
+
+# using normal thresholding (rather than inverse thresholding)
+(T, thresh) = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)
+cv2.imshow("Threshold Binary", thresh)
+
+# visualize only the masked regions in the image
+masked = cv2.bitwise_and(image, image, mask=threshInv)
+cv2.imshow("Output", masked)
+cv2.waitKey(0)
